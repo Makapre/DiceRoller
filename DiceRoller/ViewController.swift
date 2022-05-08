0f09480782dde277b4fa686ca310aebae13e08f8
Marius Preikschat <m.preikschat@googlemail.com>
2022-05-08 19:10:06 +0200

    Initial Commit

---
+++ DiceRoller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiceRoller
+//
+//  Created by Marius Preikschat on 08.05.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
